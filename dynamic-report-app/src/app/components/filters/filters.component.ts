5a218c044bb660ae000991123f230ca94eeaca5d
Banti Singh <testingvisitor896@gmail.com>
2025-05-09 11:42:18 +0530

    table group by sorting and pagination setp

--- dynamic-report-app/src/app/components/filters/filters.component.ts
+++ dynamic-report-app/src/app/components/filters/filters.component.ts
@@ -7,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrl: './filters.component.css'
 })
 export class FiltersComponent {
+  availableColumns = [
+    { name: 'id', type: 'number' },
+    { name: 'created_at', type: 'date' },
+    { name: 'amount', type: 'number' },
+    { name: 'status', type: 'string' }
+  ];
 
+  filters = [{ column: '', operator: '=', value: '' }];
+  groupBy = '';
+  sortBy = { column: '', direction: 'asc' };
+  xAxis = '';
+  yAxis = '';
+
+  addFilter() {
+    this.filters.push({ column: '', operator: '=', value: '' });
+  }
+
+  removeFilter(index: number) {
+    this.filters.splice(index, 1);
+  }
+
+  saveConfig() {
+    const config = {
+      filters: this.filters,
+      groupBy: this.groupBy,
+      sortBy: this.sortBy,
+      xAxis: this.xAxis,
+      yAxis: this.yAxis
+    };
+    console.log('Saved Config:', config);
+    // Optionally store in service or local storage
+  }
 }
