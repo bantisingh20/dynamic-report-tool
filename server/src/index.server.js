d4114d18e3b92b0996d609a7f1d1443a8246aace
bantisingh20 <124371011+bantisingh20@users.noreply.github.com>
2025-06-01 21:15:14 +0530

    test

--- server/src/index.server.js
+++ server/src/index.server.js
@@ -1,4 +1,5 @@
 const app = require('./index');
 const serverless = require('serverless-http');
 
+
 module.exports = serverless(app);
--- server/src/routes/metadata.routes.js
+++ server/src/routes/metadata.routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {  listTablesAndViews,PreviewReport,  CheckRelationAndListOfColumn , SaveReportConfiguration, ListReport, GetReportById} = require('../controller/metadata.controller.js');
 
+router.get('/', (req, res) => {
+  res.json({ message: 'Metadata API working!' });
+});
+
 //list page
 router.get('/List-Report', ListReport); 
 router.get('/report/:id', GetReportById);
